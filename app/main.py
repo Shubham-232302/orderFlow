106e52734a8fcd09418475635c67e27e2c4cd846
Shubham <sdeodhar47@gmail.com>
2026-08-25 23:45:14 +0530

    test main.py

--- app/main.py
+++ app/main.py
@@ -1,7 +1,11 @@
+
 from fastapi import FastAPI
 
-app = FastAPI()
+app = FastAPI(
+    title = "Order Flow API",
+    version = "0.1.0"
+)
 
-@app.get("/")
+@app.get("/health")
 def read_items():
     return {"message": "Hello, World!"}
