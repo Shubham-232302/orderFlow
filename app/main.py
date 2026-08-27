e6d16cde9147ef18834f24d74d8ec7c55fc7bba7
Shubham <sdeodhar47@gmail.com>
2026-08-27 21:14:23 +0530

    first api

--- app/main.py
+++ app/main.py
@@ -4,19 +4,19 @@ from sqlalchemy import text
 from sqlalchemy.orm import Session
 
 from app.db.dependencies import get_db
+from app.api.routes.users import router as users_router
 
 app = FastAPI(
     title = "Order Flow API",
     version = "0.1.0"
 )
 
+
+app.include_router(
+    users_router,
+    prefix="/api/v1"
+)
+
 @app.get("/health")
 def read_items():
     return {"message": "Hello, World!"}
-
-
-
-@app.get("/health/db")
-def database_health(db:Session = Depends(get_db)):
-    result = db.execute(text("SELECT 1"))
-    return {"database":result.scalar()}
