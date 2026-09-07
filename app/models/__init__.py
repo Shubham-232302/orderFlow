6d9511bfeb2f4e6df0958b083740d395072d370a
Shubham <sdeodhar47@gmail.com>
2026-09-07 23:13:22 +0530

    alembic

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,4 +1,6 @@
 from app.models.user import User
 from app.models.product import Product
+from app.models.orders import Order
+from app.models.order_items import OrderItem
 
-__all__ = ["User", "Product"] # important for alembic because we need SQLAlchemy's metadata to know about our models
+__all__ = ["User", "Product", "Order", "OrderItem"] # important for alembic because we need SQLAlchemy's metadata to know about our models
